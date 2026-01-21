05f3571058e1fcbf04a6cde9a5f8a67d71b2c669
Thet Hnin Hsu <thethninhsu@Thets-Air-2.lan>
2026-01-20 20:02:00 -0700

    Lab 3: add City class and update content layout

---
+++ code/app/src/main/java/com/example/listycitylab3/City.java
@@ -0,0 +1,18 @@
+package com.example.listycitylab3;
+
+public class City {
+    private String name;
+    private String provnince;
+    public City(String name, String provnince){
+        this.name = name;
+        this.province = provnince;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getProvnince(){
+        return province;
+    }
+}
